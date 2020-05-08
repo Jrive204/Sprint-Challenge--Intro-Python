55ec279885ff8da97868d8ece34a3b03d0437361
Jose Rivera <jrivera6869@gmail.com>
2020-05-08 01:36:14 -0400

    finished city reader

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -1,5 +1,8 @@
-# The following list comprehension exercises will make use of the 
-# defined Human class. 
+# The following list comprehension exercises will make use of the
+# defined Human class.
+import math
+
+
 class Human:
     def __init__(self, name, age):
         self.name = name
@@ -7,6 +10,7 @@ class Human:
 
     def __repr__(self):
         return f"<Human: {self.name}, {self.age}>"
+
 
 humans = [
     Human("Alice", 29),
@@ -39,13 +43,15 @@ print("Starts between C and G, inclusive:")
 c = []
 print(c)
 
-# Write a list comprehension that creates a list of all the ages plus 10.
+# Write a list comprehension that creates a list of all the ages plus
+# 10.
 print("Ages plus 10:")
 d = []
 print(d)
 
 # Write a list comprehension that creates a list of strings which are the name
-# joined to the age with a hyphen, for example "David-31", for all humans.
+# joined to the age with a hyphen, for example "David-31", for all
+# humans.
 print("Name hyphen age:")
 e = []
 print(e)
@@ -64,8 +70,8 @@ print("All names uppercase:")
 g = []
 print(g)
 
-# Write a list comprehension that contains the square root of all the ages.
+# Write a list comprehension that contains the square root of all the
+# ages.
 print("Square root of ages:")
-import math
 h = []
 print(h)
